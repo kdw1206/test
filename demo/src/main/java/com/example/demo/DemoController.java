616fdf8671806bab2198e05f591e1a445b9efed4
rayful <rayful@DESKTOP-OC7H8F8>
2021-05-31 17:36:13 +0900

    init

---
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,46 @@
+
+package com.example.demo;
+
+
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.miracom.oneoone.transceiverx.Transceiver;
+
+/**
+* <pre> 
+* simple : 계좌 Controller Module.
+* detail : .
+* kr.co.kospo.accounts.controller
+* |_ AccountsController.java
+* </pre>
+*
+* @company : RayfulPlus Co., Ltd.
+* @author : csc
+* @date : 2020. 10. 16. 오전 9:28:32
+* @version : 1.0
+* @History :
+* name : date : document : content
+* ------------------------------------------------------
+* csc : 2020. 10. 16. : : new.
+*/
+
+@RestController
+public class DemoController {
+
+	private Transceiver transceiver ;
+	
+	@RequestMapping(value = "test")
+	public @ResponseBody String test(@RequestParam HashMap<String, Object> reqMap) throws Exception {
+		
+		transceiver = new Transceiver(null, 0);
+		System.out.println("transceiver=="+transceiver);
+		
+		return "welcome";
+	}
+	
+}
